081ecf97d77f58bb55cd55184de70df0eeb40c47
sahilgupta221 <sg5414@rit.edu>
2020-09-09 05:18:55 -0700

    deleted all folders

--- query_worker_multiple_targets.sh
+++
@@ -1,9 +0,0 @@
-IFS=$'\n' read -d '' -r -a websites < website_list.txt ; echo ${websites[*]}
-
-for i in "${websites[@]}"
-do
-echo $i
-ranNum=$[RANDOM%10+1]
-sudo python3.6 query_workers.py -t $i -v
-sleep $ranNum
-done
