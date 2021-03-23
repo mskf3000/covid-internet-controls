1edad78bd6aede3b8ba2eac921c83c4b6a242090
n <namitamadhira81@gmail.com>
2021-03-23 12:43:36 -0400

    edits

--- worker/rtt_distance.py
+++ worker/rtt_distance.py
@@ -47,7 +47,7 @@ def ip_rtt(ip:str):
    first_city_name = source_data_dict["city"]
    second_city_name = dest_data_dict["city"]
    first_city_coordinates = f'{source_data_dict["latitude"]}, {source_data_dict["longitude"]}'
-   second_city_coordinates = f'{dest_data_dict["latitude"]}, {dest_data_dict["longitude"]}'
+   second_city_coordinates = f'{dest_location_dict["latitude"]}, {dest_location_dict["longitude"]}'
 
    # Distance between two latitudes and longitudes:
 
