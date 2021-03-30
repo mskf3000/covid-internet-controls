4761dacaf8b985af3f40029639f8f8b93e31aea1
namita <namitamadhira81@gmail.com>
2021-03-29 23:17:03 -0700

    for testing

--- workers.py
+++ workers.py
@@ -1,10 +1,38 @@
 workers = [
     {
-        "country_name": "Australia",
-        "country_code": "AU",
-        "continent": "Europe",
-        "city": "Sydney",
-        "ip": "127.0.0.1",
+        "country_name": "Japan",
+        "country_code": "JP",
+        "continent": "Asia",
+        "city": "Tokyo",
+        "ip": "210.140.10.50",
+    },
+    {
+        "country_name": "Russia",
+        "country_code": "RU",
+        "continent": "Asia",
+        "city": "Moscow",
+        "ip": "158.255.2.65",
+    },
+    {
+        "country_name": "Singapore",
+        "country_code": "SG",
+        "continent": "Asia",
+        "city": "NA",
+        "ip": "139.99.31.107",
+    },
+    {
+        "country_name": "South Africa",
+        "country_code": "SA",
+        "continent": "Africa",
+        "city": "Johannesburg",
+        "ip": "102.130.116.16"
+    },
+    {
+        "country_name": "South Korea",
+        "country_code": "SK",
+        "continent": "Asia",
+        "city": "Seoul",
+        "ip": "27.255.75.155",
     },
 ]
 
