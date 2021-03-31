0a9cf5ccb273ace3f8cea623db6b8dda30336fb7
Namita <74912909+namitamadhira@users.noreply.github.com>
2021-03-31 14:38:56 -0400

    rtt_distance combine query_worker

---
+++ rtt_distance_query_worker_combine.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+
+IFS=$'\n' read -d '' -r -a targets < /home/${USER}/covid-internet-controls/target_ips.txt ; echo ${targets[*]}
+
+for i in "${targets[@]}"
+do
+echo $i
+ranNum=$[RANDOM%5+1]
+sudo python3.6 query_workers.py -r $i -v
+sleep $ranNum
+done
+
+sleep 108
