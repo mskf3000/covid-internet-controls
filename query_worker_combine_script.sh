97d0edac0d2b52607b92a9b56043839440bef05e
Namita <74912909+namitamadhira@users.noreply.github.com>
2021-03-31 14:33:50 -0400

    Update query_worker_combine_script.sh

--- query_worker_combine_script.sh
+++ query_worker_combine_script.sh
@@ -1,30 +1,6 @@
 #!/bin/bash
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
-#IFS=$'\n' read -d '' -r -a websites < /home/${USER}/covid-internet-controls/website_list.txt ; echo ${websites[*]}
-
-#for i in "${websites[@]}"
-#do
-#echo $i
-#ranNum=$[RANDOM%5+1]
-#sudo python3.6 query_workers.py -t $i -v
-#sleep $ranNum
-#done
-
-#sleep 108
-
-#IFS=$'\n' read -d '' -r -a websites < /home/${USER}/covid-internet-controls/trigger_terms_website.txt ; echo ${websites[*]}
-#touch sahiltest.txt
-#for i in "${websites[@]}"
-#do
-#echo $i
-#ranNum=$[RANDOM%5+1]
-#sudo python3.6 query_workers.py -t $i -v
-#sleep $ranNum
-#done
-
-#sleep 108
-
 IFS=$'\n' read -d '' -r -a targets < /home/${USER}/covid-internet-controls/target_ips.txt ; echo ${targets[*]}
 
 for i in "${targets[@]}"
@@ -34,3 +10,5 @@ ranNum=$[RANDOM%5+1]
 sudo python3.6 query_workers.py -r $i -v
 sleep $ranNum
 done
+
+sleep 108
